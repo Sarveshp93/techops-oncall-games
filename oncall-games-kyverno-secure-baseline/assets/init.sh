69bb0460ffb877e937a1f6b6586cf3a83c86006e
sarvesh.pawar@logicmonitor.com <sarvesh.pawar@logicmonitor.com>
2025-09-19 13:34:08 +0530

    correction

--- oncall-games-kyverno-secure-baseline/assets/init.sh
+++ oncall-games-kyverno-secure-baseline/assets/init.sh
@@ -4,6 +4,7 @@ source /opt/k8s.sh
 log "Init Kyverno baseline"
 wait_apiserver
 ensure_ns kyverno
+ensure_ns app
 kubectl create -f https://github.com/kyverno/kyverno/releases/download/v1.15.1/install.yaml >>/opt/init.log 2>&1
 kubectl -n kyverno rollout status deploy --timeout=300s --all || true
 ensure_ns app
